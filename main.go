ae2f88f41aab1878a551e0f367d181bb772df777
wangzhumo <wangzhumoo@gmail.com>
2021-07-01 02:05:25 +0800

    [feature]config db

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	_ "com.wangzhumo.iyouku/routers"
-
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
+	defaultDB, _ := beego.AppConfig.String("defaultdb")
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", defaultDB)
+
 	beego.Run()
 }
