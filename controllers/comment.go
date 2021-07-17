93a145a6c975769d66da739b43168a646be25921
wangzhumo <wangzhumoo@gmail.com>
2021-07-17 14:03:20 +0800

    [feature]comment go

--- controllers/comment.go
+++ controllers/comment.go
@@ -68,6 +68,102 @@ func (cc *CommentController) GetCommentList() {
 	}
 }
 
+// GetGoCommentList 获取评论列表 使用协程
+// @router /comment/ex/list [*]
+func (cc *CommentController) GetGoCommentList() {
+	// 剧集id
+	episodesId, _ := cc.GetInt("episodesId")
+	// 分页信息
+	limit, _ := cc.GetInt("limit")
+	offset, _ := cc.GetInt("offset")
+
+	// empty
+	if episodesId == 0 {
+		cc.Data["json"] = ErrorResp(4001, NoEpisodesID)
+		_ = cc.ServeJSON()
+	}
+	if limit == 0 {
+		limit = 10
+	}
+
+	// get data
+	count, comments, err := models.GetCommentsByEpisodesId(episodesId, offset, limit)
+	if err != nil {
+		cc.Data["json"] = ErrorResp(4004, VideoEpisodesCommentError)
+		_ = cc.ServeJSON()
+	} else {
+		var commentInfos []CommentInfo
+		var commentInfo CommentInfo
+		// 获取评论列表中用户的信息
+		// 10 是因为 limit = 10
+		uidChan := make(chan int, 10)
+		closeChan := make(chan bool, 5)
+		taskChan := make(chan models.UserInfo, 10)
+
+		// uid 发送至 uidChannel
+		go func() {
+			for _, comment := range comments {
+				uidChan <- comment.UserId
+			}
+			close(uidChan)
+		}()
+
+		// 发起用户信息获取任务
+		for i := 0; i < 10; i++ {
+			go chanGetUserInfo(uidChan, closeChan, taskChan)
+		}
+
+		// 判断任务是否完成
+		go func() {
+			for i := 0; i < 10; i++ {
+				<-closeChan
+				// 阻塞
+			}
+			close(taskChan)
+			close(closeChan)
+		}()
+
+		// 获取taskChan中的用户数据
+		userInfoMap := make(map[int64]models.UserInfo)
+
+		// channel在for循环中，有一个特性  当channel关闭时，会跳出这个循环
+		for c := range taskChan {
+			userInfoMap[c.Id] = c
+		}
+
+		// 获取评论列表中用户的信息
+		for _, comment := range comments {
+			// 评论
+			commentInfo.Id = comment.Id
+			commentInfo.Content = comment.Content
+			commentInfo.AddTime = comment.AddTime
+			commentInfo.AddTimeTitle = common.DateFormat(comment.AddTime)
+			commentInfo.PraiseCount = comment.PraiseCount
+			commentInfo.UserId = comment.UserId
+			commentInfo.Stamp = comment.Stamp
+
+			// 获取用户信息
+			commentInfo.UserInfo, _ = userInfoMap[int64(comment.UserId)]
+			commentInfos = append(commentInfos, commentInfo)
+		}
+
+		cc.Data["json"] = SucceedResp(0, RequestOk, commentInfos, count)
+		_ = cc.ServeJSON()
+	}
+}
+
+// Channel 获取用户信息
+func chanGetUserInfo(uidChan chan int, closeChan chan bool, taskChan chan models.UserInfo) {
+	for uid := range uidChan {
+		userInfo, err := models.GetCacheUserInfo(int64(uid))
+		// 发送回去
+		if err == nil {
+			taskChan <- userInfo
+		}
+	}
+	closeChan <- true
+}
+
 // InsertComment 写入一条评论
 // @router /comment/save [*]
 func (cc *CommentController) InsertComment() {
---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,172 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:BasicController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:BasicController"],
+		beego.ControllerComments{
+			Method:           "ChannelRegion",
+			Router:           "/channel/region",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:BasicController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:BasicController"],
+		beego.ControllerComments{
+			Method:           "ChannelType",
+			Router:           "/channel/type",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"],
+		beego.ControllerComments{
+			Method:           "GetGoCommentList",
+			Router:           "/comment/ex/list",
+			AllowHTTPMethods: []string{"*"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"],
+		beego.ControllerComments{
+			Method:           "InsertCommentEx",
+			Router:           "/comment/ex/save",
+			AllowHTTPMethods: []string{"*"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"],
+		beego.ControllerComments{
+			Method:           "GetCommentList",
+			Router:           "/comment/list",
+			AllowHTTPMethods: []string{"*"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:CommentController"],
+		beego.ControllerComments{
+			Method:           "InsertComment",
+			Router:           "/comment/save",
+			AllowHTTPMethods: []string{"*"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:TopController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:TopController"],
+		beego.ControllerComments{
+			Method:           "GetChannelTop",
+			Router:           "/channel/top",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:TopController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:TopController"],
+		beego.ControllerComments{
+			Method:           "GetChannelType",
+			Router:           "/type/top",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "Login",
+			Router:           "/login/do",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "SaveRegister",
+			Router:           "/register/save",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "SendPushMessage",
+			Router:           "/send/message",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "ChannelAdvert",
+			Router:           "/channel/advert",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "ChannelHotList",
+			Router:           "/channel/hot",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "ChannelRecommendByRegion",
+			Router:           "/channel/recommend/region",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "ChannelRecommendByType",
+			Router:           "/channel/recommend/type",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "ChannelVideoByParams",
+			Router:           "/channel/video",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "GetVideoEpisode",
+			Router:           "/video/episode",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"] = append(beego.GlobalControllerRouter["com.wangzhumo.iyouku/controllers:VideoController"],
+		beego.ControllerComments{
+			Method:           "GetVideoInfo",
+			Router:           "/video/info",
+			AllowHTTPMethods: []string{"*"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
